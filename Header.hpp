72561da57e1f966323cb6875d3c6ca9d9d062786
Bioticcc <biotic@MiniMe.>
2025-01-07 20:40:21 -0800

    first commit

---
+++ Header.hpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>  
+#include <ostream>
+#include <string>      
+#include <vector>    
+#include <map>     
+#include <algorithm>   
+#include <stdexcept>  
+#include <fstream>
+#include <sstream>
+
+using std::ifstream;
+using std::ofstream;
+using std::cout;
+using std::cin;
+using std::string;
+using std::vector;
+using std::endl;
+using namespace std;
+
---
+++ main.cpp
@@ -0,0 +1,6 @@
+
+#include "Header.hpp"
+
+int main(){
+    cout << "testing 1 2 3!" << endl;
+}
