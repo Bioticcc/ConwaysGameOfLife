7d5a901cdf8aff5c0eb0afda4c8fcdfc2eec6218
Bioticcc <adamward.bio@gmail.com>
2025-01-10 13:37:32 -0800

    1/10

--- MyProject/src/game.cpp
+++ MyProject/src/game.cpp
@@ -1,1 +1,27 @@
 #include "Header.hpp"
+
+class Game {
+
+    private:
+        //Grid grid;
+        //Unit unitArray;
+        
+    public:
+        Game(){};
+        //still need gets/sets/copys/deconstructors
+
+        void runApp(){
+            sf::RenderWindow window(sf::VideoMode(900,900), "Conway");
+            while (window.isOpen())
+            {
+                sf::Event event;
+                while (window.pollEvent(event))
+                {
+                    if (event.type == sf::Event::Closed)
+                        window.close();
+                }
+                window.clear(sf::Color::White);
+                window.display();
+            }
+        }
+};
--- MyProject/src/main.cpp
+++ MyProject/src/main.cpp
@@ -1,33 +1,26 @@
 
 #include "Header.hpp"
+#include "game.cpp"
 
+/*
+UPDATING CMAKE FOR RUNNING PROGRAM:
+ 1) cmake ..
+ 2) make
+ 3) ./MyProject
+
+PUSHING TO GITHUB
+ 1) git add .
+ 2) git commit -m "message"
+ 3) git status
+ 4) git push 
+ 5) git push origin main
+*/
 
 int main()
 {
     // Create a window with title "My SFML Window" and size 800x600
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My SFML Window");
-
-    // Main loop to keep the window open until it is closed
-    while (window.isOpen())
-    {
-        sf::Event event;
-        // Process events
-        while (window.pollEvent(event))
-        {
-            // Close window if the close event is triggered
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        // Clear the screen with a color (black)
-        window.clear(sf::Color::Green);
-
-        // Draw everything here (nothing in this case)
-        // window.draw(...);  // You can draw stuff here
-
-        // Display the contents of the window
-        window.display();
-    }
+    Game Conway;
+    Conway.runApp();
 
     return 0;
 }
